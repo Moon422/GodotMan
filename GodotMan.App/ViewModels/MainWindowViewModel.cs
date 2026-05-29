0fd02ea6c835e6cda7004f54e92cd65110e78b84
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2026-05-29 22:38:19 +0600

    removed them again

--- GodotMan.App/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace GodotMan.App.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- GodotMan.App/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace GodotMan.App.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
