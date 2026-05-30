45a23dfdbcb2f3a1d31b15f68ec309437590d974
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2026-05-30 20:53:23 +0600

    removed avalonia godotman.app

--- GodotMan.App/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using ReactiveUI;
-
-namespace GodotMan.App.ViewModels;
-
-public abstract class ViewModelBase : ReactiveObject, IActivatableViewModel
-{
-    public ViewModelActivator Activator { get; } = new();
-}
